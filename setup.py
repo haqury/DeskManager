7b3c1fb2fa2543d10f56f111bc3c6f0729d3d2d8
haqur <haqur@HAOSIT>
2024-01-01 13:17:01 +0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='DeskManager',
+    version='1.0',
+    description='A useful module',
+    author='Author Name',
+    author_email='haqury@gmail.com',
+    packages=['DeskManager'],  # same as name
+    install_requires=[],  # external packages as dependencies
+)
